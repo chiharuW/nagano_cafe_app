0a62d90eafe1d1f4a8c70ac6c494a961c8d6dbf1
EC2 Default User <ec2-user@ip-172-31-90-125.ec2.internal>
2022-05-26 13:56:37 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
-  
+
+  get 'items' => 'public/items#index'
+
+  namespace :admin do
+   get 'items' => 'admin/items#index'
+  end
+
 end
