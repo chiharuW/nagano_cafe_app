98dc83db8c140c8f125dbc5c2fcf98331c5a8e22
EC2 Default User <ec2-user@ip-172-31-90-125.ec2.internal>
2022-06-04 23:40:12 +0000

    Update

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,3 +1,23 @@
 class Public::CustomersController < ApplicationController
-  
+   before_action :ensure_correct_customer, only:[:edit, :update]
+
+  def show
+
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+
+private
+  def customer_params
+   params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :is_active)
+  end
+
+  def ensure_correct_customer
+   @customer = Customer.find(params[:id])
+   redirect_to(customer_path(current_customer.id)) unless @customer == current_customer
+  end
+
 end
