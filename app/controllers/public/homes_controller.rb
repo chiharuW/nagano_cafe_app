27ef616e5d0a50fb2d4d5a1bbb8fd97df7074776
EC2 Default User <ec2-user@ip-172-31-90-125.ec2.internal>
2022-06-04 09:46:05 +0000

    Update

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,3 +1,7 @@
 class Public::HomesController < ApplicationController
-  
+
+ def top
+  @items =  Item.limit(5).order(:created_at)
+ end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,30 @@
 Rails.application.routes.draw do
   #顧客用
   #URL　/customers/sign_in...
-  devise_for :customers,skip: [:passwords], controllers: {
+  devise_for :customers, skip: [:passwords], controllers: {
    registrations: "public/registrations",
    sessions: "public/sessions"
   }
 
   #管理者用
   #URL /admin/sign_in...
-  devise_for :admin,skip: [:registrations, :passwords], controllers: {
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
    sessions: "admin/sessions"
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
-   get 'items' => 'public/items#index'
+   get '/' => 'public/homes#top'
    resources :customers, only:[:show, :edit, :update, :unsubscribe, :withdraw]
+   resources :sessions, only:[:new, :create, :destroy]
 
   namespace :admin do
-   get 'top' => 'homes#top'
+   root to: 'homes#top'
    resources :genres, only: [:edit, :index, :create, :update]
    resources :items, only: [:new, :show, :edit, :index, :create, :update]
    resources :customers, only: [:index, :show, :edit, :update]
-   resources :orders, only: [:index, :show, :edit, :update]   
+   resources :orders, only: [:index, :show, :edit, :update]
+   resources :sessions, only:[:new, :create, :destroy]
   end
 
 end
